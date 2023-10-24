5b06cdb84b022394cd8ba5ec83eeefe32c82742c
Diego200202 <diegoeugenio61@gmail.com>
2023-10-24 14:43:55 +0200

    Lista Simple

--- EDA/src/Examen_1/Ontener_Lista_Simetrica/Main.java
+++ EDA/src/Examen_1/Ontener_Lista_Simetrica/Main.java
@@ -2,6 +2,13 @@ package Examen_1.Ontener_Lista_Simetrica;
 
 public class Main {
 	public static void main(String[] args) {
+		SimpleLinkedList<Integer> l = new SimpleLinkedList<Integer>();
+		l.anadirNodo(8);
+		l.anadirNodo(3);
+		l.anadirNodo(4);
+		l.anadirNodo(1);
 		
+		System.out.println("Lista simple:");
+		l.imprimirLista();
 	}
 }
--- EDA/src/Examen_1/Ontener_Lista_Simetrica/SimpleLinkedList.java
+++ EDA/src/Examen_1/Ontener_Lista_Simetrica/SimpleLinkedList.java
@@ -1,5 +1,32 @@
 package Examen_1.Ontener_Lista_Simetrica;
 
-public class SimpleLinkedList {
-
+public class SimpleLinkedList<T> {
+	Node<Integer> first;
+	
+	public void anadirNodo(Integer pData) {
+		if(this.first == null) {
+			this.first = new Node<Integer>(pData);
+			this.first.next = null;
+		}else {
+			Node<Integer> actual = this.first;
+			while(actual.next!=null) {
+				actual = actual.next;
+			}
+			actual.next = new Node<Integer>(pData);
+			actual.next.next = null;
+		}
+	}
+	
+	public void imprimirLista() {
+		if(this.first == null) {
+			System.out.println("La lista esta vacia");
+		}else {
+			Node<Integer> actual = this.first;
+			while(actual.next!=null){
+				System.out.print(actual.data +", ");
+				actual = actual.next;
+			}
+			System.out.println(actual.data +".\n");
+		}
+	}
 }
