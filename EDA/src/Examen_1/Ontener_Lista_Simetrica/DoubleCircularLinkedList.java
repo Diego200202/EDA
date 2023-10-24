369ea0259cd5db557fa8aef78fed303833d3bac1
Diego200202 <diegoeugenio61@gmail.com>
2023-10-24 14:29:12 +0200

    Clases hechas y nodos completos

---
+++ EDA/src/Examen_1/Ontener_Lista_Simetrica/DoubleCircularLinkedList.java
@@ -0,0 +1,5 @@
+package Examen_1.Ontener_Lista_Simetrica;
+
+public class DoubleCircularLinkedList {
+
+}
---
+++ EDA/src/Examen_1/Ontener_Lista_Simetrica/Node.java
@@ -0,0 +1,10 @@
+package Examen_1.Ontener_Lista_Simetrica;
+
+public class Node<T> {
+	T data;
+	Node<T> next;
+	
+	public Node(T pData) {
+		this.data = pData;
+	}
+}
