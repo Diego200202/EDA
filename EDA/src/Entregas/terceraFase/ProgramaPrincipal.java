51e92a0011ad46e5d48f9be3d3e97b609f753c57
Diego200202 <diegoeugenio61@gmail.com>
2023-11-29 15:12:57 +0100

    carpeta cuarta entrega hecha

--- EDA/src/Entregas/terceraFase/ProgramaPrincipal.java
+++ EDA/src/Entregas/terceraFase/ProgramaPrincipal.java
@@ -1,14 +1,15 @@
 package Entregas.terceraFase;
 
 import java.util.*;
+
 public class ProgramaPrincipal {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         // Crear una instancia de ListaPelis
         ListaPelis lista = new ListaPelis();
-    
+
         // Cargar los ficheros y procesar las películas
-    
+
         // Crear una instancia de Graph
         Graph graph = new Graph();
 
@@ -16,20 +17,20 @@ public class ProgramaPrincipal {
         String dir = sc.nextLine();
 
         graph.cargarFicheros(dir, lista);
-    
+
         // Crear el grafo a partir de la lista de películas
         graph.crearGrafo(lista);
-    
+
         // Imprimir el grafo
         System.out.println("Grafo:");
-        //graph.print();
-    
+        graph.print();
+
         // Verificar si dos actores están conectados
         String actor1 = "Brad Pitt";
         String actor2 = "Cate Blanchett";
-    
+
         boolean estanConectados = graph.estanConectados(actor1, actor2);
         System.out.println("\n¿Están conectados " + actor1 + " y " + actor2 + "? " + estanConectados);
     }
-    
+
 }
